f7405634f1baa6f88140cee90123757ecfca94d1
Minh Huy <minhhuydtt2k5@gmail.com>
2025-10-15 14:11:44 +0700

    update doctor prescriptions

--- frontend/src/routes/AppRoutes.js
+++ frontend/src/routes/AppRoutes.js
@@ -23,6 +23,8 @@ import DoctorScheduleManagement from "../pages/Doctor/DoctorScheduleManagement";
 import DoctorAppointmentList from "../pages/Doctor/DoctorAppointmentList";
 import DoctorProfile from "../pages/Doctor/DoctorProfile";
 import DoctorAvailableSlotManagement from "../pages/Doctor/DoctorAvailableSlotManagement";
+import DoctorPrescriptions from "../pages/Doctor/DoctorPrescriptions";
+import PrescriptionForm from "../pages/Doctor/PrescriptionForm";
 
 // Patient pages
 import PatientAppointmentBooking from "../pages/Patient/PatientAppointmentBooking";
@@ -95,6 +97,8 @@ const AppRoutes = () => {
           element={<DoctorAvailableSlotManagement />}
         />
         <Route path="appointments" element={<DoctorAppointmentList />} />
+        <Route path="prescriptions" element={<DoctorPrescriptions />} />
+        <Route path="prescriptions/new" element={<PrescriptionForm />} />
         <Route path="profile" element={<DoctorProfile />} />
       </Route>
 
